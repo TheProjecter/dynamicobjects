6fff94e3ab4c33c7870f4bb79e4bb5c24e9205bf
Stephan Zehrer <stephan.zehrer@gmail.com>
2010-10-25 21:43:14 +0200

    Inital Version of test case

---
+++ net.zehrer.no2.semantic.editor.tests/src/net/zehrer/no2/sematic/editor/model/EditorUtil.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Stephan Zehrer and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Stephan Zehrer - initial API and implementation
+ *******************************************************************************/
+
+package net.zehrer.no2.sematic.editor.model;
+
+import net.zehrer.no2.semantic.editor.model.AbstractNode;
+import net.zehrer.no2.semantic.editor.model.CompositeNode;
+import net.zehrer.no2.semantic.editor.model.EditorFactory;
+import net.zehrer.no2.semantic.editor.model.LeafNode;
+
+
+public class EditorUtil {
+	
+	public static String logGraph (CompositeNode node) {
+		
+		StringBuilder buffer = new StringBuilder(Math.max(16, node.getTotalLength()));
+
+		logGraph(node, buffer);
+		return buffer.toString();
+	}
+	
+
+	public static void logGraph(AbstractNode node, StringBuilder buffer) {
+		if (node instanceof LeafNode) {
+			buffer.append("LeafNode (" + node.getOffset() + ":" + node.getLength() + ") : ");
+			buffer.append(((LeafNode) node).getText() + "\n");
+		}
+		else {
+			CompositeNode parent = (CompositeNode) node;
+			buffer.append("CompositeNode (" + parent.getOffset() + ":" + parent.getLength() + ")\n");
+			for (AbstractNode childNode : parent.getChildren()) {
+				logGraph(childNode, buffer);
+			}
+		}
+	}
+
+	public static LeafNode getNode (String text) {
+		LeafNode leafNode = EditorFactory.eINSTANCE.createLeafNode();
+		leafNode.setText(text);
+		return leafNode;
+	}
+
+
+}
---
+++ net.zehrer.no2.semantic.editor.tests/src/net/zehrer/no2/sematic/editor/model/TestModelUseCases.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Stephan Zehrer and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Stephan Zehrer - initial API and implementation
+ *******************************************************************************/
+
+package net.zehrer.no2.sematic.editor.model;
+
+import junit.framework.TestCase;
+import net.zehrer.no2.semantic.editor.TextModelManager;
+import net.zehrer.no2.semantic.editor.adapter.NodeContentAdapter;
+import net.zehrer.no2.semantic.editor.model.CompositeNode;
+import net.zehrer.no2.semantic.editor.model.EditorFactory;
+import net.zehrer.no2.semantic.editor.model.LeafNode;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Stephan Zehrer
+ */
+public class TestModelUseCases extends TestCase{
+	
+	final static String testText = "Hello World";
+	
+	/**
+	 * The fixture for this model test case.
+	 */
+	protected CompositeNode fixture = null;
+
+	/**
+	 * Constructs a new test case with the given name.
+	 */
+	public TestModelUseCases(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this model test case.
+	 */
+	protected void setFixture(CompositeNode fixture) {
+		this.fixture = fixture;
+	}
+	
+	/**
+	 * Returns the fixture for this model test case.
+	 */
+	protected CompositeNode getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		CompositeNode node = TextModelManager.modelInit(testText);
+		
+		NodeContentAdapter.createAdapterAndAddToNode(node);
+		
+		setFixture(node);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+	
+	// ------- TEST CODE ------
+	
+	public void testSerialize() {
+
+		CompositeNode node = getFixture();
+		
+		assertEquals(testText, node.serialize());
+		
+	}
+
+	
+	public void testAdd() {
+
+		CompositeNode node = getFixture();
+		
+		LeafNode leafNode = EditorUtil.getNode("12345");
+		
+		System.out.println(EditorUtil.logGraph(node));
+		
+		node.getChildren().add(leafNode);
+		
+		System.out.println(EditorUtil.logGraph(node));
+		
+		leafNode = EditorUtil.getNode("Test");
+		
+		node.getChildren().add(1, leafNode);
+		
+		System.out.println(EditorUtil.logGraph(node));
+		
+		//System.out.printf(node.serialize());
+	}
+}
