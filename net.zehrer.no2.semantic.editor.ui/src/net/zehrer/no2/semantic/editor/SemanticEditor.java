f47395868e37d01fcd34b614b8194e992df136f9
Stephan Zehrer <stephan.zehrer@gmail.com>
2010-11-14 18:03:00 +0100

    move disposal code add OutlineListener refactor  getAdapter inital version of the OutlineSelection Listiner

--- net.zehrer.no2.semantic.editor.ui/src/net/zehrer/no2/semantic/editor/SemanticEditor.java
+++ net.zehrer.no2.semantic.editor.ui/src/net/zehrer/no2/semantic/editor/SemanticEditor.java
@@ -3,6 +3,7 @@ package net.zehrer.no2.semantic.editor;
 import net.zehrer.no2.semantic.editor.coloring.ColorManager;
 import net.zehrer.no2.semantic.editor.outline.OutlineContentProvider;
 import net.zehrer.no2.semantic.editor.outline.OutlineLabelProvider;
+import net.zehrer.no2.semantic.editor.outline.OutlineSelectionListener;
 import net.zehrer.no2.semantic.editor.text.SemanticDocumentProvider;
 import net.zehrer.no2.ui.outline.GenericContentOutlinePage;
 import net.zehrer.no2.ui.outline.IEditor;
@@ -18,14 +19,19 @@ import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
 import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
 import org.eclipse.emf.edit.ui.action.EditingDomainActionBarContributor;
 import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+import org.eclipse.ui.views.properties.IPropertySheetPage;
 
-public class SemanticEditor extends AbstractDecoratedTextEditor implements IEditor { // extends TextEditor {
+public class SemanticEditor extends TextEditor implements IEditor { // extends TextEditor {
 
 	private ColorManager colorManager;
 	private GenericContentOutlinePage fOutlinePage;
+	
+	private OutlineSelectionListener outlineSelectionListener;
 	
 	private EditingDomain editingDomain;
 	private ComposedAdapterFactory adapterFactory;
@@ -40,12 +46,13 @@ public class SemanticEditor extends AbstractDecoratedTextEditor implements IEdit
 		
 		// TODO: inject?
 		setDocumentProvider(new SemanticDocumentProvider(this.editingDomain.getResourceSet()));
+
+		showHighlightRangeOnly(false);
+		
+		this.outlineSelectionListener = new OutlineSelectionListener(this);
 	}
 
-	public void dispose() {
-		colorManager.dispose();
-		super.dispose();
-	}
+
 	
 	public void doSave(IProgressMonitor monitor) {
 		super.doSave(monitor);
@@ -56,29 +63,39 @@ public class SemanticEditor extends AbstractDecoratedTextEditor implements IEdit
 	//TODO SourceViewerDecorationSupport
 	
 	@SuppressWarnings("rawtypes")
-	public Object getAdapter(Class required) {
-		if (IContentOutlinePage.class.equals(required)) {
-			if (fOutlinePage == null) {
-				fOutlinePage= createOutlinePage();
-			}
-			return fOutlinePage;
+	@Override
+	public Object getAdapter(Class key) {
+		if (key.equals(IContentOutlinePage.class)) {
+			return getOutlinePage();
+		} else if (key.equals(IPropertySheetPage.class)) {
+		  return getPropertySheetPage();
+//		} else if (key.equals(IGotoMarker.class)) {
+//			return this;
+		} else {
+			return super.getAdapter(key);
 		}
-		return super.getAdapter(required);
 	}
 
-	private GenericContentOutlinePage createOutlinePage() {
+	private IContentOutlinePage getOutlinePage() {
+		// TODO use injection to avoid singelton
 		
-		GenericContentOutlinePage outlinePage = new GenericContentOutlinePage(this);
-		outlinePage.setContentProvider(new OutlineContentProvider());
-		outlinePage.setLabelProvider(new OutlineLabelProvider());
-		
-		IEditorInput input = getEditorInput();
-		if (input != null) {
-			// Get the input by the DocumentProvider 
-			outlinePage.setInput(getDocumentProvider().getDocument(input));
+		if (fOutlinePage == null) {
+			fOutlinePage = new GenericContentOutlinePage(this);
+			fOutlinePage.setContentProvider(new OutlineContentProvider());
+			fOutlinePage.setLabelProvider(new OutlineLabelProvider());
+			
+			IEditorInput input = getEditorInput();
+			if (input != null) {
+				// Get the input by the DocumentProvider 
+				fOutlinePage.setInput(getDocumentProvider().getDocument(input));
+			}
 		}
 		
-		return outlinePage;
+		return fOutlinePage;
+	}
+	
+	private IPropertySheetPage getPropertySheetPage() {
+		return null;
 	}
 	
 	private void initializeEditingDomain() {
@@ -105,5 +122,25 @@ public class SemanticEditor extends AbstractDecoratedTextEditor implements IEdit
 		
 	}
 	
-
+	
+	// --------------------
+	
+	/**
+	 * @see AbstractTextEditor#createPartControl(Composite)
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+		
+		getSite().getPage().addSelectionListener(outlineSelectionListener);
+	}
+	
+	@Override
+	public void dispose() {
+		colorManager.dispose();
+		
+		getSite().getPage().removeSelectionListener(outlineSelectionListener);
+		
+		super.dispose();
+	}
 }
