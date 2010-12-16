2de6678091f165f5fa69d1e6b9e85c88f7d4cd3f
Stephan Zehrer <stephan.zehrer@gmail.com>
2010-12-16 08:49:21 +0100

    working on the group function

--- net.zehrer.no2.semantic.editor/src/net/zehrer/no2/semantic/editor/model/java/TextModelUtil.java
+++ net.zehrer.no2.semantic.editor/src/net/zehrer/no2/semantic/editor/model/java/TextModelUtil.java
@@ -15,6 +15,7 @@ package net.zehrer.no2.semantic.editor.model.java;
 
 import java.util.List;
 
+import net.zehrer.common.interval.EIntInterval;
 import net.zehrer.no2.semantic.editor.model.AbstractNode;
 import net.zehrer.no2.semantic.editor.model.CompositeNode;
 import net.zehrer.no2.semantic.editor.model.EditorFactory;
@@ -29,19 +30,18 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jdt.core.dom.ThisExpression;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.TypedRegion;
 
-
 public class TextModelUtil {
-	
+
 	private static final char[] separator = System.getProperty("line.separator").toCharArray();
 
 	private static void checkArgument(AbstractNode abstractParserNode) {
 		int classifierID = abstractParserNode.eClass().getClassifierID();
 		if (classifierID != EditorPackage.COMPOSITE_NODE && classifierID != EditorPackage.LEAF_NODE) {
-			throw new IllegalArgumentException("Illegal subtype of AbstarctParserNode "
-					+ abstractParserNode.eClass().getName());
+			throw new IllegalArgumentException("Illegal subtype of AbstarctParserNode " + abstractParserNode.eClass().getName());
 		}
 	}
 
@@ -94,24 +94,24 @@ public class TextModelUtil {
 
 	public static String serialize(AbstractNode _this) {
 		if (_this instanceof LeafNodeImpl)
-			return serialize((LeafNodeImpl)_this);
+			return serialize((LeafNodeImpl) _this);
 		checkArgument(_this);
 		StringBuilder buffer = new StringBuilder(Math.max(16, _this.getLength()));
-//		EList<LeafNode> leafNodes = _this.getLeafNodes();
-//		for (LeafNode leafNode : leafNodes) {
-//			buff.append(leafNode.getText());
-//		}
+		// EList<LeafNode> leafNodes = _this.getLeafNodes();
+		// for (LeafNode leafNode : leafNodes) {
+		// buff.append(leafNode.getText());
+		// }
 		serialize(_this, buffer);
 		return buffer.toString();
 	}
-	
+
 	public static void serialize(AbstractNode node, StringBuilder buffer) {
 		if (node instanceof LeafNode)
 			buffer.append(((LeafNode) node).getText());
 		else {
 			CompositeNode parent = (CompositeNode) node;
 			EList<AbstractNode> children = parent.getChildren();
-			for(int i = 0; i < children.size(); i++) {
+			for (int i = 0; i < children.size(); i++) {
 				serialize(children.get(i), buffer);
 			}
 		}
@@ -145,12 +145,12 @@ public class TextModelUtil {
 		addAllSyntaxErrors(compositeNodeImpl, basicEList);
 		return basicEList;
 	}
-	
+
 	public static void addAllSyntaxErrors(CompositeNode node, BasicEList<SyntaxError> result) {
 		if (node.getSyntaxError() != null)
 			result.add(node.getSyntaxError());
 		List<AbstractNode> children = node.getChildren();
-		for(int i = 0; i< children.size(); i++) {
+		for (int i = 0; i < children.size(); i++) {
 			AbstractNode child = children.get(i);
 			if (child instanceof LeafNode) {
 				if (child.getSyntaxError() != null)
@@ -176,29 +176,29 @@ public class TextModelUtil {
 	 * @param abstractNode
 	 * @return
 	 */
-//	public static int getOffset(AbstractNode abstractNode) {
-//		
-//		if (abstractNode instanceof LeafNode)
-//			return abstractNode.getTotalOffset();
-//	
-//		final CompositeNode node = (CompositeNode) abstractNode;
-//		
-//		for (int i = 0; i < node.getChildren().size(); i++) {	
-//			final AbstractNode child = node.getChildren().get(i);
-//			
-//			if (child instanceof CompositeNode) {
-//				if (hasLeafNodes((CompositeNode) child))
-//					return getOffset(child);
-//			} else {
-//				final LeafNode leaf = (LeafNode) child;
-//				if (!leaf.isHidden())
-//					return leaf.getTotalOffset();
-//			}
-//		}
-//		
-//		// every child node is a hidden node, return total offset
-//		return abstractNode.getTotalOffset();
-//	}
+	// public static int getOffset(AbstractNode abstractNode) {
+	//
+	// if (abstractNode instanceof LeafNode)
+	// return abstractNode.getTotalOffset();
+	//
+	// final CompositeNode node = (CompositeNode) abstractNode;
+	//
+	// for (int i = 0; i < node.getChildren().size(); i++) {
+	// final AbstractNode child = node.getChildren().get(i);
+	//
+	// if (child instanceof CompositeNode) {
+	// if (hasLeafNodes((CompositeNode) child))
+	// return getOffset(child);
+	// } else {
+	// final LeafNode leaf = (LeafNode) child;
+	// if (!leaf.isHidden())
+	// return leaf.getTotalOffset();
+	// }
+	// }
+	//
+	// // every child node is a hidden node, return total offset
+	// return abstractNode.getTotalOffset();
+	// }
 
 	private static boolean hasLeafNodes(CompositeNode child) {
 		for (AbstractNode node : child.getChildren()) {
@@ -225,8 +225,8 @@ public class TextModelUtil {
 			if (child instanceof CompositeNode)
 				return getLine(child);
 			final LeafNode leaf = (LeafNode) child;
-//			if (!leaf.isHidden())
-				return leaf.getTotalLine();
+			// if (!leaf.isHidden())
+			return leaf.getTotalLine();
 		}
 		// every child node is a hidden node, return total line
 		return abstractNode.getTotalLine();
@@ -236,24 +236,25 @@ public class TextModelUtil {
 	 * @param abstractNode
 	 * @return
 	 */
-//	public static int getLength(AbstractNode abstractNode) {
-//		
-//		if (abstractNode instanceof LeafNode)
-//			return abstractNode.getTotalLength();
-//		
-//		final CompositeNode node = (CompositeNode) abstractNode;
-//		for (int i = node.getChildren().size() - 1; i >= 0; i--) {
-//			
-//			final AbstractNode child = node.getChildren().get(i);
-//			if (child instanceof CompositeNode)
-//				return child.getOffset() - abstractNode.getOffset() + child.getLength();
-//			
-//			if (!((LeafNode) child).isHidden())
-//				return child.getTotalOffset() - abstractNode.getOffset() + child.getTotalLength();
-//		}
-//		// every child node is a hidden node, return total length
-//		return abstractNode.getTotalLength();
-//	}
+	// public static int getLength(AbstractNode abstractNode) {
+	//
+	// if (abstractNode instanceof LeafNode)
+	// return abstractNode.getTotalLength();
+	//
+	// final CompositeNode node = (CompositeNode) abstractNode;
+	// for (int i = node.getChildren().size() - 1; i >= 0; i--) {
+	//
+	// final AbstractNode child = node.getChildren().get(i);
+	// if (child instanceof CompositeNode)
+	// return child.getOffset() - abstractNode.getOffset() + child.getLength();
+	//
+	// if (!((LeafNode) child).isHidden())
+	// return child.getTotalOffset() - abstractNode.getOffset() +
+	// child.getTotalLength();
+	// }
+	// // every child node is a hidden node, return total length
+	// return abstractNode.getTotalLength();
+	// }
 
 	/**
 	 * @param abstractNode
@@ -267,195 +268,283 @@ public class TextModelUtil {
 			final AbstractNode child = node.getChildren().get(i);
 			if (child instanceof CompositeNode)
 				return endLine(child);
-//			if (!((LeafNode) child).isHidden())
-				return child.totalEndLine();
+			// if (!((LeafNode) child).isHidden())
+			return child.totalEndLine();
 		}
 		// every child node is a hidden node, return total endLine
 		return abstractNode.totalEndLine();
 	}
-	
+
 	/**
-	 * Return the related leaf node 
+	 * Return the related leaf node
+	 * 
 	 * @param _this
 	 * @param offset
 	 * @return
 	 */
 	public static LeafNode getLeafNode(AbstractNode _this, int offset) {
-		
+
 		LeafNode result = null;
-		
+
 		// 1st: check of offset starts in this node
 		if (!checkNodeOffset(_this, offset))
-			return null;  // exit condition
-		
+			return null; // exit condition
+
 		// 2nd: in yes analyze deeper
 		if (_this instanceof LeafNode) {
-			result =  (LeafNode) _this;
+			result = (LeafNode) _this;
 		} else {
 			CompositeNode parent = (CompositeNode) _this;
-			
+
 			EList<AbstractNode> children = parent.getChildren();
 			for (AbstractNode abstractNode : children) {
-				result = getLeafNode (abstractNode, offset);
-				if (result != null) break;
-			}	
+				result = getLeafNode(abstractNode, offset);
+				if (result != null)
+					break;
+			}
 		}
-		
+
 		return result;
 	}
-	
 
 	/**
-	 * Return a list of leaf nodes beginning with "start offset" and end with "end offset" including all leaf nodes in between 
-	 * @param _this starting composite node
-	 * @param sOffset - is the start offset
-	 * @param eOffset - is the end offset
+	 * Return a list of leaf nodes beginning with "start offset" and end with
+	 * "end offset" including all leaf nodes in between
+	 * 
+	 * @param _this
+	 *            starting composite node
+	 * @param sOffset
+	 *            - is the start offset
+	 * @param eOffset
+	 *            - is the end offset
 	 * @return the list of leaf nodes
 	 */
-	public static EList<LeafNode> getLeafNodes(AbstractNode _this, int sOffset, int eOffset ) {
-	
+	public static EList<LeafNode> getLeafNodes(AbstractNode _this, int sOffset, int eOffset) {
+
 		BasicEList<LeafNode> result = new BasicEList<LeafNode>();
-		
+
 		addLeafNodes(_this, sOffset, eOffset, result);
-		
+
 		return result;
 	}
 
 	/**
-	 * Return the related leaf node 
+	 * Return the related leaf node
+	 * 
 	 * @param _this
 	 * @param offset
 	 * @return
 	 */
-	public static int addLeafNodes(AbstractNode _this, int indexOffset, int endOffset, EList<LeafNode> list ) {
-		
+	public static int addLeafNodes(AbstractNode _this, int indexOffset, int endOffset, EList<LeafNode> list) {
+
 		// 1st: check the offset for start and end in this node
 		if (!checkNodeOffset(_this, indexOffset)) {
-			  return indexOffset;  // search for start
+			return indexOffset; // search for start
 		}
-		
-		int result = indexOffset; 
-		
+
+		int result = indexOffset;
+
 		// 2nd: in yes analyze deeper
 		if (_this instanceof LeafNode) {
 			LeafNode leafNode = (LeafNode) _this;
 			list.add(leafNode);
-			
+
 			result = leafNode.getOffset() + leafNode.getLength();
 			if (result > endOffset)
-				 result = -1;   // exit condition
-		 
+				result = -1; // exit condition
+
 		} else {
 			CompositeNode parent = (CompositeNode) _this;
-			
+
 			EList<AbstractNode> children = parent.getChildren();
 			for (AbstractNode abstractNode : children) {
-				result = addLeafNodes (abstractNode, result , endOffset , list);
-				if (result == -1) break;
-			}	
+				result = addLeafNodes(abstractNode, result, endOffset, list);
+				if (result == -1)
+					break;
+			}
 		}
-		
-		return result;  
+
+		return result;
 	}
 
-	
-	
 	/**
 	 * Return true if offset within _this node
+	 * 
 	 * @param _this
 	 * @param offset
 	 */
-	public static boolean checkNodeOffset (AbstractNode _this, int offset) {
-		
+	public static boolean checkNodeOffset(AbstractNode _this, int offset) {
+
 		int fOffset = _this.getOffset();
 		int fLength = _this.getLength();
-		
+
 		// fOffset <= offset <= (fOffset + fLength)
-		
+
 		if ((fOffset <= offset)) {
-			if (offset < fOffset + fLength ) {
+			if (offset < fOffset + fLength) {
 				return true;
 			}
-		} 
-		
+		}
+
 		return false;
 	}
 
-	
-	public static void update ( AbstractNode _this, DocumentEvent update) {
-		updateModel (_this,update, null);
+	public static void update(AbstractNode _this, DocumentEvent update) {
+		updateModel(_this, update, null);
 	}
-	
-	public static DocumentEvent updateModel ( AbstractNode _this, DocumentEvent event, CompositeNode parent) {
+
+	public static DocumentEvent updateModel(AbstractNode _this, DocumentEvent event, CompositeNode parent) {
 
 		// 1st: check of event starts in this node
 		if (!checkNodeOffset(_this, event.getOffset()))
 			return event;
-		
+
 		DocumentEvent result = event;
-		
+
 		// 2nd: distinguish type
 		if (_this instanceof LeafNode) {
-			result = updateModel ((LeafNode) _this, result, parent);
+			result = updateModel((LeafNode) _this, result, parent);
 		} else {
 			CompositeNode fParent = (CompositeNode) _this;
-			
+
 			EList<AbstractNode> children = fParent.getChildren();
 			for (AbstractNode abstractNode : children) {
-				result = updateModel (abstractNode, result, fParent);
-				
-				if (result == null) break;
+				result = updateModel(abstractNode, result, fParent);
+
+				if (result == null)
+					break;
 			}
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * @param _this
 	 * @param event
-	 * @return either null if even full covered in this node or 
+	 * @return either null if even full covered in this node or
 	 */
-	public static DocumentEvent updateModel ( LeafNode _this, DocumentEvent event, CompositeNode parent) {
-		
+	public static DocumentEvent updateModel(LeafNode _this, DocumentEvent event, CompositeNode parent) {
+
 		DocumentEvent result = null;
-		
+
 		int fOffset = _this.getOffset();
 		int fLength = _this.getLength();
-		
-		int startIndex = event.getOffset() - fOffset;  
-		int endIndex = startIndex + event.getLength(); 
-		
+
+		int startIndex = event.getOffset() - fOffset;
+		int endIndex = startIndex + event.getLength();
+
 		// if the change range bigger as the current LeafNode?
 		if (endIndex > fOffset + fLength) {
-			
+
 			int sOffset = fOffset + fLength;
 			int sLength = endIndex - fOffset + fLength;
-			
+
 			// TODO fix SPLIT event
 			// BUG !!!
-			
-			result = new DocumentEvent(event.getDocument(),sOffset,sLength,"");
-			
+
+			result = new DocumentEvent(event.getDocument(), sOffset, sLength, "");
+
 			endIndex = fOffset + fLength;
 		}
-		
+
 		// replace text
 		StringBuffer buffer = new StringBuffer(_this.getText());
 		buffer.replace(startIndex, endIndex, event.getText());
-	
+
 		// replace leafNode to notify the adapter
 		// TODO: or update the NodeContentAdapter;
 		_this.setText(buffer.toString());
 		int index = parent.getChildren().indexOf(_this);
 		parent.getChildren().set(index, _this);
-		
+
 		return result;
-	}	
+	}
 
 	public static TypedRegion createTypedRegion(AbstractNode _this, String type) {
 		return new TypedRegion(_this.getOffset(), _this.getLength(), type);
 	}
+
+	// -----
+
+	public static EList<AbstractNode> group(AbstractNode _this, EIntInterval selection) {
+
+		if (_this instanceof CompositeNode) {
+			CompositeNode fParent = (CompositeNode) _this;
+			
+			BasicEList<AbstractNode> intersectingNodes = null;
+			
+			EList<AbstractNode> children = fParent.getChildren();
+			
+			// 1st iterate through the graph and collect intersecting nodes
+			for (AbstractNode abstractNode : children) {
+				
+				// 1st intersection / subset test
+				if (abstractNode.intersects(selection)) {
+					
+					// only of the selection intersects do ....
+					if (abstractNode instanceof CompositeNode) {
+						if (selection.isSubsetOf(abstractNode)) {
+							// go more deeper in the graph
+							group(abstractNode,selection);
+							break;
+						} 
+					}
+					
+				
+					if (intersectingNodes == null)
+						intersectingNodes = new BasicEList<AbstractNode>();  // auto init???
+					intersectingNodes.add(abstractNode);	
+				}
+			}
+			
+			// 2nd group intersecting nodes
+			// TODO: check edges and group
+		}
+		
+
+		return null;
+		
+//		result = group((LeafNode) _this, selection);
+//		} else {
+		
+		// 1st: check of intersection
+//		if (!_this.intersects(selection))
+//			return null;
+
+//		
+//
+//		if (_this.isSubsetOf(selection)) {
+//			result.add(_this);
+//		}
+
+	}
 	
-	
+	public static void group(CompositeNode _this, EIntInterval selection) {
+		
+	}
+
+	// ***********
+	// xxx xxxxxxxxxxx xx
+	// xxx xxxxxxxxxxx xx
+
+	public static EList<AbstractNode> group(LeafNode _this, EIntInterval selection) {
+
+		BasicEList<AbstractNode> result = new BasicEList<AbstractNode>();
+
+		add(result, (LeafNode) _this.leftComplementRelativeTo(selection));
+
+		result.add((LeafNode) _this.intersect(selection));
+
+		add(result, (LeafNode) _this.rightComplementRelativeTo(selection));
+
+		return result;
+
+	}
+
+	// addes only if object is not null
+	private static <T> void add(EList<T> list, T object) {
+		if (object != null)
+			list.add(object);
+	}
 }
